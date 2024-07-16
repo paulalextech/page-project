f19afb1ffb8fb88a2d5ce35ec61b15b59b9a8888
paulalextech <psteane123@hotmail.com>
2024-07-16 17:09:02 +0100

    added skeleton info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
-<h1>Software Developer</h1>
+<h1 class="text-5xl font-semibold text-center">Software Developer</h1>
+<div class="bg-white">
+	<p>Welcome to my portfolio built using Svelte and SvelteKit.</p>
+	<p>
+		I'm a Full-Stack Software Developer. I love building things with tech and retro video games!
+	</p>
+</div>
+
+<div class="text-5xl pt-5">"this is where tech stack logos will go"</div>
 
 <style lang="postcss">
 	:global(html) {
