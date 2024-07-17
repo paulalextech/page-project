20ce73e195e63acf1377cd1187b68cd0740d554d
paulalextech <psteane123@hotmail.com>
2024-07-17 11:45:01 +0100

    iconify hero img

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,24 @@
 <h1 class="text-5xl font-semibold text-center">Software Developer</h1>
-<div class="bg-white">
+
+<div class="bg-white p-3">
 	<p>Welcome to my portfolio built using Svelte and SvelteKit.</p>
 	<p>
 		I'm a Full-Stack Software Developer. I love building things with tech and retro video games!
 	</p>
 </div>
+<div>
+	<img
+		class="rounded-full width={300} height={300}"
+		src="../assets/hero.svg"
+		alt="cartoon profile"
+	/>
+</div>
 
 <div class="text-5xl pt-5">"this is where tech stack logos will go"</div>
+
+<div>
+	<iconify-icon icon="mdi:home"></iconify-icon>
+</div>
 
 <style lang="postcss">
 	:global(html) {
