4d691055ce8fb7c81fec646378eb8f64478c9568
paulalextech <psteane123@hotmail.com>
2024-07-18 11:19:40 +0100

    re-order layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,30 @@
 <h1 class="text-5xl font-semibold text-center">Software Developer</h1>
 
-<div class="bg-white p-3">
+<div class="w-full flex flex-col items-center justify-center my-10">
+	<div class="md:w-1/2 md:mt-2">
+		<img
+			class="rounded-full width={300} height={300}"
+			src="../assets/hero.svg"
+			alt="cartoon profile"
+		/>
+	</div>
+</div>
+
+<div class=" md:mt-2 md:w-3/5 bg-white p-3">
 	<p>Welcome to my portfolio built using Svelte and SvelteKit.</p>
 	<p>
 		I'm a Full-Stack Software Developer. I love building things with tech and retro video games!
 	</p>
 </div>
-<div>
-	<img
-		class="rounded-full width={300} height={300}"
-		src="../assets/hero.svg"
-		alt="cartoon profile"
-	/>
-</div>
 
-<div class="text-5xl pt-5">"this is where tech stack logos will go"</div>
+<!-- <div class="text-5xl pt-5 text-center">"this is where tech stack logos will go"</div> -->
 
-<div>
-	<iconify-icon icon="mdi:home"></iconify-icon>
+<div class="text-5xl pt-5 text-center">
+	<iconify-icon icon="mdi:react"></iconify-icon>
+	<iconify-icon icon="mdi:react"></iconify-icon>
+	<iconify-icon icon="mdi:react"></iconify-icon>
+	<iconify-icon icon="mdi:react"></iconify-icon>
+	<iconify-icon icon="mdi:react"></iconify-icon>
 </div>
 
 <style lang="postcss">
