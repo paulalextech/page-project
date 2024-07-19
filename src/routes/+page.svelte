2722c9dee76b4bd72b7411afc4295e955c111fc7
paulalextech <psteane123@hotmail.com>
2024-07-19 11:42:50 +0100

    css for home section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
 	<!-- <h1 class="text-5xl font-semibold text-center">Software Developer</h1> -->
 
 	<div
-		class="h-screen w-full flex flex-col text-center items-center justify-center my-10 py-16 sm:py-32 md:flex-row md:space-x-4 md:text-left md:py-28"
+		class="h-full w-full flex flex-col text-center items-center justify-center my-10 py-16 sm:py-32 md:flex-row md:space-x-4 md:text-left md:py-28"
 	>
-		<div class="md:w-1/2 md:mt-2">
+		<div class=" md:w-1/2 md:mt-2">
 			<img
 				class="rounded-full"
 				src="../assets/hero.svg"
@@ -15,16 +15,18 @@
 		</div>
 	</div>
 
-	<div class=" md:mt-2 md:w-3/5 bg-white p-3">
-		<p>Welcome to my portfolio built using Svelte and SvelteKit.</p>
-		<p>
+	<div class="md:mt-2 md:w-3/5 p-3">
+		<h1 class="font-bold sm:text-6xl text-4xl mt-6 md:text-6xl md:mt-0">
+			Welcome to my portfolio built using Svelte and SvelteKit.
+		</h1>
+		<p class="text-xl mt-4 mb-2 md:text-3xl">
 			I'm a Full-Stack Software Developer. I love building things with tech and retro video games!
 		</p>
 	</div>
 
 	<!-- <div class="text-5xl pt-5 text-center">"this is where tech stack logos will go"</div> -->
 
-	<div class="text-5xl pt-5 text-center">
+	<div class="flex flex-row gap-4 mb-4 text-5xl pt-5 text-center">
 		<iconify-icon icon="mdi:react"></iconify-icon>
 		<iconify-icon icon="mdi:react"></iconify-icon>
 		<iconify-icon icon="mdi:react"></iconify-icon>
