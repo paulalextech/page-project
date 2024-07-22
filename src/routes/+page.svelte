d59f21f4883b16587e9b108a6f6a423a0071d494
paulalextech <psteane123@hotmail.com>
2024-07-22 10:57:58 +0100

    added individual component files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <section id="home">
-	<!-- <h1 class="text-5xl font-semibold text-center">Software Developer</h1> -->
+	<h1 class="text-5xl font-semibold text-center">Software Developer</h1>
+
+	<!-- Navbar -->
+	<div class="container max-w-6xl mx-auto px-6 py-4 md:px-0">
+		<!-- nav.flex.items-center.justify-between.font-bold -->
+	</div>
 
 	<div
-		class="h-full w-full flex flex-col text-center items-center justify-center my-10 py-16 sm:py-32 md:flex-row md:space-x-4 md:text-left md:py-28"
+		class="w-full flex items-center justify-center sm:py-32 md:flex-row md:space-x-4 md:text-left md:py-28"
 	>
-		<div class=" md:w-1/2 md:mt-2">
+		<div class="md:w-1/2 md:mt-2">
 			<img
 				class="rounded-full"
 				src="../assets/hero.svg"
