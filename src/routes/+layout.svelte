43922f14fe06866c44501312494091dfea48b736
paulalextech <psteane123@hotmail.com>
2024-07-23 14:39:00 +0100

    adjusted profile pic created links to documentation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,9 +10,9 @@
 
 <section
 	id="home"
-	class="w-full h-screen flex items-center justify-center sm:py-32 md:flex-row md:space-x-4 md:text-left md:py-28"
+	class="w-full flex flex-row items-center justify-center sm:py-32 md:flex-row md:space-x-4 md:text-left md:py-28"
 >
-	<div class="md:w-1/2 md:mt-2">
+	<div class="flex flex-row justify-center items-center md:w-1/2 md:mt-2">
 		<img
 			class="rounded-full p-3"
 			src="../assets/hero.svg"
@@ -24,7 +24,10 @@
 
 	<div class="md:mt-2 md:w-3/5 p-3">
 		<h1 class="font-bold sm:text-6xl text-4xl mt-6 md:text-6xl md:mt-0">
-			Welcome to my portfolio built using <span class="text-orange-600">Svelte</span> and
+			Welcome to my portfolio built using <a href="https://svelte.dev/"
+				><span class="text-orange-600">Svelte</span></a
+			>
+			and
 			<span class="text-orange-600">SvelteKit.</span>
 		</h1>
 		<!-- <p class="text-xl mt-4 mb-2 md:text-3xl">
