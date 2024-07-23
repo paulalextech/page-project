7d7efc80873dd4c01b93c22470933ed840617e78
paulalextech <psteane123@hotmail.com>
2024-07-23 15:16:44 +0100

    add some icons. need to check open new tab for nav links

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,8 +2,8 @@
 	<ul
 		class="flex items-center justify-between space-x-10 my-1 mr-1 text-3xl md:flex font-semibold lowercase"
 	>
-		<li class=" hover:text-orange-500 mr-20"><a href="/">Home</a></li>
-		<li class=" hover:text-orange-500 mr-20"><a href="/">Projects</a></li>
-		<li class=" hover:text-orange-500 mr-20"><a href="/">Contact</a></li>
+		<li class=" hover:text-orange-500 mr-20"><a href="#" target="_blank">Home</a></li>
+		<li class=" hover:text-orange-500 mr-20"><a href="/" target="_blank">Projects</a></li>
+		<li class=" hover:text-orange-500 mr-20"><a href="/" target="_blank">Contact</a></li>
 	</ul>
 </nav>
