f5624af2dc003cbd5dbf0a0fbc3bbfa55c71bc3a
paulalextech <psteane123@hotmail.com>
2024-07-29 11:20:27 +0100

    added navbar styling

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -3,7 +3,9 @@
 		class="flex items-center justify-between space-x-10 my-1 mr-1 text-3xl md:flex font-semibold lowercase"
 	>
 		<li class=" hover:text-orange-500 mr-20"><a href="#" target="_blank">Home</a></li>
-		<li class=" hover:text-orange-500 mr-20"><a href="/" target="_blank">Projects</a></li>
+		<li class=" hover:text-orange-500 mr-20">
+			<a href="projects.svelte" target="_blank">Projects</a>
+		</li>
 		<li class=" hover:text-orange-500 mr-20"><a href="/" target="_blank">Contact</a></li>
 	</ul>
 </nav>
