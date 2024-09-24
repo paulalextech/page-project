e9dc1eb449bf08a924d3d94ccc0996196e3980d5
paulalextech <psteane123@hotmail.com>
2024-09-24 13:59:24 +0100

    description to projects

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -4,5 +4,12 @@
 </script>
 
 <section>
-	<h1>projects</h1>
+	<h1>Projects</h1>
+
+	<div>
+		<p>
+			Check out some of my projects. I am currently working on number of educational platforms' for
+			the Education and Government sector.
+		</p>
+	</div>
 </section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import Main from '../components/Main.svelte';
 	import Navbar from '../components/Navbar.svelte';
 	import Page from './+page.svelte';
+	import Projects from '../components/Projects.svelte';
 </script>
 
 <Main />
